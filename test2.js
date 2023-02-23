78dd2fb7cc4d96b627df4681afd00f931a4a9a81
SabrinaMouginot <s.mouginot@yahoo.fr>
2023-02-23 22:35:09 +0100

    ajout d images et de style

---
+++ test2.js
@@ -0,0 +1,10 @@
+document.write("<img class='error' src='error404.jpg' alt='image de papier toilette vide avec erreur 404'></img>")
+
+document.write ('<img class="joke" src="joke.png" alt="crotte"><br>');
+
+
+
+
+for(let x = 0; x < 20; x ++ ) {
+document.write ('<div class="crotte"> 🟤<br></div>');
+}
